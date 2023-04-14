e9d395edce41e9899838030ceb66faf7ce3b399c
Leo Tamminen <lettam@utu.fi>
2023-04-15 00:00:05 +0300

    tehtävässä annetut testisyötteet

--- teht4.4/tests/listHelper.test.js
+++ teht4.4/tests/listHelper.test.js
@@ -9,50 +9,76 @@ test('dummy returns one', () => {
 
 const listWithOneBlog = [
   {
-    'title': 'Jees',
-    'author': 'Jussi',
-    'url': 'http://localhost:3001',
-    'likes': 41
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
   }
 ]
 
-const manyBlogs = [
+const blogs = [
   {
-    'title': 'Blogipostaus',
-    'author': 'Eetu',
-    'url': 'http://localhost:3003/api/blogs',
-    'likes': 90
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0
   },
   {
-    'title': 'ESLint',
-    'author': 'Jaakko',
-    'url': 'https://jastpa.utugit.fi/webprog/osa3/validointi_ja_es_lint',
-    'likes': 7
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
   },
   {
-    'title': 'Oispa',
-    'author': 'Aatu',
-    'url': 'https://oispa.kievinkanaa.com/',
-    'likes': 27
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0
   },
   {
-    'title': 'Fullstack',
-    'author': 'Iivari',
-    'url': 'https://jastpa.utugit.fi/webprog/osa4/sovelluksen_rakenne_ja_testauksen_alkeet',
-    'likes': 16
+    _id: '5a422b891b54a676234d17fa',
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+    __v: 0
   }
 ]
 
 describe('totalLikes', () => {
-  test('of empty list returns zero', () => {
+  test('of empty list is zero', () => {
     expect(listHelper.totalLikes([])).toBe(0)
   })
 
-  test('of one blog is the value likes in the blog itself', () => {
-    expect(listHelper.totalLikes(listWithOneBlog)).toBe(41)
+  test('when list has only one blog equals the likes of that', () => {
+    expect(listHelper.totalLikes(listWithOneBlog)).toBe(5)
   })
 
-  test('of many blogs is calculated right', () => {
-    expect(listHelper.totalLikes(manyBlogs)).toBe(140)
+  test('of a bigger list is calculated right', () => {
+    expect(listHelper.totalLikes(blogs)).toBe(7+5+12+10+0+2)
   })
 })
